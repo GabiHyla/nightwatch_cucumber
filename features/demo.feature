bac81914bb5a632c2789249f9b0aba0f707a0723
Gabi <gabi.hyla@gmail.com>
2018-01-16 13:35:12 +0000

    update

--- features/demo.feature
+++ features/demo.feature
@@ -1,6 +1,52 @@
 Feature: This is a demo scenario
+@test1
+Scenario Outline: Open a page and check for an image
 
-Scenario: Open a page and check for an image
+Given I open the site "https://www.facebook.com/"
+When I enter an "<email>" address
+And I enter a "<password>"
+And I select the "input[value='Log In']" button 
+Then URL contains "/?sk=welcome"
 
-Given I open the site "https://www.maxharding4.com/"
-Then the element "h1.site-title" is present
+Examples:
+| email | password |
+| cocoliso1981@hotmail.co.uk | Qwerty1234 |
+#| nredrctest+16@gmail.com | 1234Qwerty|
+
+@test2
+Scenario Outline: New User Registration
+
+Given I open the site "https://www.facebook.com/"
+When I enter first name as "<firstName>" in the input form
+And I enter surname as "<surname>" in the input form
+And I register an "<email>" address
+And I re-enter an "<email>" address
+And I enter a "<password>" in the input form 
+Then I enter the date of birth as "<dobDay>", "<dobMon>", "<dobYear>"
+And I select female gender type
+And I select the "#u_0_13" button
+
+Examples:
+| firstName | surname | email | password | dobDay | dobMon | dobYear | 
+| Claudia  | Smith  | nredrctest@gmail.com | lalaland1234 | 20 | 11 | 1990 |
+
+@test3
+Scenario Outline: New User Registration
+
+Given I open the site "https://www.facebook.com/"
+When I enter first name as "<firstName>" in the input form
+And I enter surname as "<surname>" in the input form
+And I register an "<email>" address
+And I re-enter an "<email>" address
+And I enter a "<password>" in the input form 
+And I select the gender type "<gender>"
+Then I enter the date of birth as "<dobDay>", "<dobMon>", "<dobYear>"
+When I wait 
+And I select the "#u_0_13" button
+
+Examples:
+| firstName | surname | email | password | dobDay | dobMon | dobYear | gender |
+| Mario  | Bross  | nredrctest1@gmail.com | doncaster1234 | 15 | 10 | 1981 | male |
+#| Barbara  | Johnes | nredrctest2@gmail.com | bird1234 | 20 | 10 | 1990 | female |
+
+
--- features/step_definitions/demo.js
+++ features/step_definitions/demo.js
@@ -15,6 +15,103 @@ defineSupportCode(({Given, Then, When}) => {
     });
 
 
+    When(/^I enter an "([^"]*)?" address$/, (email) => {
+        return client
+            .clearValue('input#email.inputtext')
+            .setValue('input#email.inputtext', email)
+            //.setValue('input#pass.inputtext', password)
+    });
+
+    When(/^I enter a "([^"]*)?"$/, (password) => {
+        return client
+            .clearValue('input#pass.inputtext')
+            .setValue('input#pass.inputtext', password)
+    });
+
+    When(/^I select the "([^"]*)?" button$/, (button) => {
+        return client
+            .click(button)
+          
+    });
+
+    Then(/^URL contains "([^"]*)?"$/, (url) => {
+        return client
+            .assert.urlContains(url);
+    });
+    
 
 
+    When(/^I enter first name as "([^"]*)?" in the input form$/, (firstName) => {
+        return client
+            .setValue('input[name="firstname"]', firstName)
+      
+    });   
+
+    When(/^I enter surname as "([^"]*)?" in the input form$/, (surname) => {
+        return client
+            .setValue('input[name="lastname"]', surname)
+      
+    });  
+
+    When(/^I register an "([^"]*)?" address$/, (email) => {
+        return client
+            .setValue('input[name="reg_email__"]', email)
+    
+    });
+
+    When(/^I re-enter an "([^"]*)?" address$/, (email) => {
+        return client
+            .setValue('input[name="reg_email_confirmation__"]', email)
+    
+    });
+            
+    When(/^I enter a "([^"]*)?" in the input form$/, (password) => {
+        return client
+             .setValue('input[name="reg_passwd__"]', password)
+    });
+
+    When(/^I enter the date of birth as "([^"]*)?", "([^"]*)?", "([^"]*)?"$/, (day, month, year) => {
+
+        let dd = `select[name='birthday_day'] option[value="${day}"]`;
+        let mm = `select[name='birthday_month'] option[value="${month}"]`;
+        let yy = `select[name='birthday_year'] option[value="${year}"]`;
+
+        return client
+            .click(dd)
+            .click(mm)
+            .click(yy)
+    });
+
+    When(/^I select female gender type$/, () => {
+        return client
+        .click('#u_0_b')
+    });
+
+    When(/^I select the gender type "([^"]*)?"$/, (genderType) => {
+
+        if (genderType == "female") {
+            return client
+                .waitForElementPresent('#u_0_b')
+                .click('#u_0_b')
+            
+        }
+
+        if (genderType == "male") {
+            return client
+                .waitForElementPresent('#u_0_c')
+                .click('#u_0_c')
+            
+        }
+    });
+
+    When(/^I wait$/,()=> {
+        client.pause(10000);
+    });
+
+});
+
+defineSupportCode(function({After}) {
+    After(function() {
+        return client.end();
+    });
 });
